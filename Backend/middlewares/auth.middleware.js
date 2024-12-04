f1fc2ef1dc83af7a7656e50c77cfa3739775ae09
Suhail Ahmed <suhailquazi08@gmail.com>
2024-12-04 18:49:10 +0530

    Driver authentication complete

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -2,6 +2,7 @@ const userModel = require("../models/user.model")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken");
 const blacklistTokenModel = require("../models/blacklistTokenModel");
+const driverModel = require("../models/driver.model");
 
 
 module.exports.userAuth = async (req,res,next) => {
@@ -10,7 +11,7 @@ module.exports.userAuth = async (req,res,next) => {
         return res.status(401).json({message: "Unauthorized"})
     }
 
-    const isBlacklisted = await userModel.findOne({token : token})
+    const isBlacklisted = await blacklistTokenModel.findOne({token : token})
     if(isBlacklisted){
         return res.status(401).json({message : "unauthorized"})
     }
@@ -24,5 +25,25 @@ module.exports.userAuth = async (req,res,next) => {
         return next()
     } catch (error) {
         return res.status(401).json({message: "Unauthorized"})
+    }
+}
+
+module.exports.driverAuth = async(req,res,next)=> {
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+    if(!token){
+        return res.status(401).json({message: 'unauthorized'})
+    }
+   
+    const isBlacklisted = await blacklistTokenModel.findOne({token : token})
+    if(isBlacklisted){
+        return res.status(401).json({message: 'unauthorized'})
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET )
+        const driver = await driverModel.findById(decoded._id)
+        req.driver = driver
+        return next()
+    } catch (error) {
+        return res.status(401).json({message: "unauthorized"})
     }
 }
--- Backend/routes/driver.routes.js
+++ Backend/routes/driver.routes.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const {body} = require("express-validator")
 const driverController = require("../controllers/driver.controller")
+const authMiddleware = require("../middlewares/auth.middleware")
 
 
-router.post('/register', [
+router.post('/signup', [
     body('email').isEmail().withMessage('Invalid Email'),
     body('fullName.firstName').isLength({min:3}).withMessage("firstName is must be at least 3 characters long"),
     body('password').isLength({min:6}).withMessage('password is must be ata least 6 characters long'),
@@ -13,5 +14,14 @@ router.post('/register', [
     body('vehicle.capacity').isInt({min: 1}). withMessage("capacity must be at least 1"),
     body('vehicle.vehicleType').isIn(['car', 'motorcycle', 'auto']).withMessage('Invalid VehicleType')
 ], driverController.registerDriver)
+
+router.post("/login", [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("password").isLength({min:6}).withMessage("password must be at least 6 characters long")
+], driverController.loginDriver)
+
+router.get("/profile", authMiddleware.driverAuth, driverController.getDriverProfile)
+
+router.get("/logout", authMiddleware.driverAuth, driverController.logoutDriver)
 
 module.exports = router
